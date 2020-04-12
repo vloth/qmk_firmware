a3ba0052915ad1494bfe9ed516317153564ddb5d
imchipwood <imchipwood@gmail.com>
2020-04-11 19:28:05 -0700

    adding some commented code that will be tested later

--- keyboards/dumbpad/v0x_right/config.h
+++ keyboards/dumbpad/v0x_right/config.h
@@ -5,3 +5,10 @@
 #undef LAYER_INDICATOR_LED_1
 #define LAYER_INDICATOR_LED_0 B1
 #define LAYER_INDICATOR_LED_1 B3
+
+/*
+#undef ENCODERS_PAD_A
+#undef ENCODERS_PAD_B
+#define ENCODERS_PAD_A { D4 }
+#define ENCODERS_PAD_B { D0 }
+*/
